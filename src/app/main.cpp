0753a8fece856a3782ab5f8ee131b44f1cb110d9
Goldenboylife <hwidong0102@naver.com>
2026-05-07 10:16:29 +0900

    reset codebase

--- src/app/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "app/golden_slam_app.hpp"
-#include <iostream>
-
-int main(int argc, char** argv)
-{
-    std::cout << "start" << std::endl;
-    GoldenSlamApp app(argc,argv);
-    return app.run();
-}
